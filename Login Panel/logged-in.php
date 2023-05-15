994de68c5298dc25841f8ed0491462ca7c861918
adrian1715 <adrian40001@gmail.com>
2023-05-15 11:50:29 -0300

    logged-in quick update

--- Login Panel/logged-in.php
+++ Login Panel/logged-in.php
@@ -11,7 +11,7 @@ if (isset($_SESSION['username']) && isset($_SESSION['password'])) {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Welcome</title>
 
     <style>
       body {
